08a677109e583a936de0802d9f074c9a677397e8
w2ptr <43069332+w2ptr@users.noreply.github.com>
2021-07-29 12:30:18 +0200

    Add MIDI track chunk definitions and types

--- source/mididi/def.d
+++ source/mididi/def.d
@@ -36,3 +36,234 @@ enum TrackFormat : ushort {
     sequential = 2,
 }
 
+/**
+A track event is one of the following:
+- a MIDI event, meaning it carries around any message (channel or system),
+except system exclusive messages;
+- a system exclusive event, which offers an escape to transmit arbitrary bytes;
+- a meta event, meaning it carries around other meta information.
+
+Use `isMIDIEvent()`, `isSysExEvent()` and `isMetaEvent()` to find which kind of
+event it is from its status byte.
+
+See also:
+    `mididi.types.TrackEvent`; `mididi.types.MIDIEvent`,
+    `mididi.types.SysExEvent` and `mididi.types.MetaEvent`.
+*/
+bool isMIDIEvent(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte != 0xF0 && statusByte != 0xF7 && statusByte != 0xFF;
+}
+/// ditto
+bool isSysExEvent(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte == 0xF0 || statusByte == 0xF7;
+}
+/// ditto
+bool isMetaEvent(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte == 0xFF;
+}
+
+/**
+`isChannelMessage()` and `isSystemMessage()` are used to find if a MIDI event
+is a channel message or a system message.
+
+In turn, a channel message can be either a channel voice message or a channel
+mode message. A system message can be either a system common message or a
+system realtime message.
+*/
+bool isChannelMessage(ubyte statusByte) @nogc nothrow pure @safe {
+    immutable b = cast(ubyte) (statusByte >> 4);
+    return b >= 0x8 && b <= 0xE;
+}
+/// ditto
+bool isSystemMessage(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte >= 0xF0 && statusByte <= 0xFE; // TODO: 0xFF? 0xFE? which one?
+}
+
+/**
+`ChannelMessageType` enumerates the possible types of channel messages.
+
+The underlying value of this enumeration is the four upper bits of the status
+byte (and always starts with a 1 bit). After all, a channel message has the
+message type in the four upper bits and a channel identifier in the four lower
+bits of the status byte.
+
+The functions `isChannelVoiceMessage()` and `isChannelModeMessage()` need the
+data bytes in addition to the status byte to identify if a channel message is a
+voice message or a mode message, because some status byte values overlap (see
+for example `ChannelMessageType.controlChangeOrMode`).
+
+See also:
+    `mididi.def.isChannelMessage`
+*/
+enum ChannelMessageType : ubyte {
+    ///
+    noteOff = 0x8,
+
+    ///
+    noteOn = 0x9,
+
+    ///
+    polyphonicKeyPressure = 0xA,
+
+    /**
+    NOTE: this kind of channel message can be either a Control Change message
+    or a Channel Mode message, depending on the data bytes. Use
+    `isChannelModeMessage()` to find out which it is.
+    */
+    controlChangeOrMode = 0xB,
+
+    ///
+    programChange = 0xC,
+
+    ///
+    channelPressure = 0xD,
+
+    ///
+    pitchWheelChange = 0xE,
+}
+
+/// ditto
+bool isChannelVoiceMessage(ubyte statusByte, ubyte[2] dataBytes) @nogc nothrow pure @safe {
+    immutable b = cast(ubyte) (statusByte >> 4);
+    if (b == 0xB) {
+        return dataBytes[0] <= 0x77;
+    }
+    return b >= 0x8 && b <= 0xE;
+}
+/// ditto
+bool isChannelModeMessage(ubyte statusByte, ubyte[2] dataBytes) @nogc nothrow pure @safe {
+    immutable b = cast(ubyte) (statusByte >> 4);
+    return b == 0xB && dataBytes[0] >= 0x78;
+}
+
+/**
+`SystemMessageType` enumerates the possible types of system message. Some of
+these are system common messages, others are system realtime messages.
+
+If for a `ubyte x` we have that `isSystemMessage(x)` is true, then it can be
+safely cast to `SystemMessageType` using `cast(SystemMessageType) x`.
+
+A system message is either a a "system common message" or a "system real-time
+message". You can use `isSystemCommonMessage(x)` and
+`isSystemRealTimeMessage(x)` to identify which is true, or compare to the enum
+members to find out the exact message type.
+
+See also:
+    `mididi.def.isSystemMessage`
+*/
+enum SystemMessageType : ubyte {
+    /**
+    This event can give any type of information specific to the manufacturer.
+
+    A system exclusive message can consist of several packets, where each
+    packet is placed in one event. Each event then starts with the byte `0xF7`,
+    and the final event is also terminated by `0xF7`.
+    */
+    systemExclusive = 0xF0,
+
+    ///
+    songPositionPointer = 0xF2,
+
+    ///
+    songSelect = 0xF3,
+
+    ///
+    tuneRequest = 0xF6,
+
+    ///
+    endOfExclusive = 0xF7,
+
+    ///
+    timingClock = 0xF8,
+
+    ///
+    start = 0xFA,
+
+    ///
+    continue_ = 0xFB,
+
+    ///
+    stop = 0xFC,
+
+    ///
+    activeSensing = 0xFE,
+}
+
+/// ditto
+bool isSystemCommonMessage(SystemMessageType type) @nogc nothrow pure @safe {
+    return isSystemCommonMessage(cast(ubyte) type);
+}
+/// ditto
+bool isSystemCommonMessage(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte >= 0xF0 && statusByte <= 0xF7;
+}
+/// ditto
+bool isSystemRealTimeMessage(SystemMessageType type) @nogc nothrow pure @safe {
+     return isSystemRealTimeMessage(cast(ubyte) type);
+}
+/// ditto
+bool isSystemRealTimeMessage(ubyte statusByte) @nogc nothrow pure @safe {
+    return statusByte >= 0xF8 && statusByte <= 0xFE; // TODO: 0xFF? 0xFE? which one?
+}
+
+/**
+`MetaEventType` enumerates the possible meta event types.
+
+Cast to `ubyte` to get the underlying value.
+
+Note:
+    do not use a value of this type in a `final switch`, because it might have
+    a value that is not identified in this enum, since values can always be
+    added later.
+See also:
+    `mididi.def.isMetaEvent`
+*/
+enum MetaEventType : ubyte {
+    /// 0x00
+    sequenceNumber = 0x00,
+
+    /// 0x01
+    text = 0x01,
+
+    /// 0x02
+    copyright = 0x02,
+    // TODO: this event should come in the first track, before any other event
+
+    /// 0x03
+    sequenceName = 0x03,
+
+    /// 0x04
+    instrumentName = 0x04,
+
+    /// 0x05
+    lyric = 0x05,
+
+    /// 0x06
+    marker = 0x06,
+
+    /// 0x07
+    cuePoint = 0x07,
+
+    /// 0x20
+    midiChannelPrefix = 0x20,
+
+    /// 0x2F
+    endOfTrack = 0x2F,
+    // TODO: must come at end
+
+    /// 0x51
+    setTempo = 0x51,
+
+    /// 0x54
+    smpteOffset = 0x54,
+    // TODO: must come before any event with nonzero delta-time
+
+    /// 0x58
+    timeSignature = 0x58,
+
+    /// 0x59
+    keySignature = 0x59,
+
+    /// 0x7F
+    sequencerSpecific = 0x7F,
+}
--- source/mididi/types.d
+++ source/mididi/types.d
@@ -14,6 +14,8 @@ module mididi.types;
 import mididi.def;
 
 /**
+`HeaderChunk` is the first chunk in a MIDI file. It gives information about the
+format of the other chunks.
 */
 struct HeaderChunk {
     /**
@@ -97,3 +99,243 @@ struct TimeDivision {
     }
 }
 
+/**
+A track chunk is a chunk that stores the actual data. It contains a number of
+track events.
+*/
+struct TrackChunk {
+    ///
+    TrackEvent[] events;
+}
+
+/**
+A `TrackEvent` is a delta time coupled with either a MIDI event, a system
+exclusive event or a meta event.
+
+`TrackEvent` has an underlying union that uses the status byte to discriminate
+(which cannot be changed after construction). Use `asMIDIEvent()`,
+`asSysExEvent()` or `asMetaEvent()` to access the underlying values from the
+union.
+
+See the documentation of `mididi.def.isMIDIEvent()` to see what each type of
+message means. Note: system exclusive events ARE also semantically system
+common messages. The confusing thing is that all other system common messages
+are syntactically MIDI events, but system exclusive events are not MIDI events.
+*/
+struct TrackEvent {
+    /**
+    Preconditions:
+        `statusByte` must be a valid status byte for a MIDI event, and not for
+        a system exclusive event
+    */
+    this(int deltaTime, ubyte statusByte, MIDIEvent event) @nogc nothrow pure @trusted
+    in (isMIDIEvent(statusByte)) {
+        this.deltaTime = deltaTime;
+        _statusByte = statusByte;
+        _midiEvent = event;
+    }
+    /**
+    Preconditions:
+        `statusByte` must be a valid status byte for a system exclusive event
+    */
+    this(int deltaTime, ubyte statusByte, SysExEvent event) @nogc nothrow pure @trusted
+    in (isSysExEvent(statusByte)) {
+        this.deltaTime = deltaTime;
+        _statusByte = statusByte;
+        _sysExEvent = event;
+    }
+    /**
+    Preconditions:
+        `statusByte` must be a MIDI event status byte (meaning its value must
+        be `0xFF`)
+    */
+    this(int deltaTime, ubyte statusByte, MetaEvent event) @nogc nothrow pure @trusted
+    in (isMetaEvent(statusByte)) {
+        this.deltaTime = deltaTime;
+        _statusByte = statusByte;
+        _metaEvent = event;
+    }
+
+    /**
+    `deltaTime` is the time between the previous event and this event.
+    */
+    int deltaTime;
+
+    /**
+    Returns:
+        this event's status byte (see `TrackEvent`'s' documentation)
+    Note:
+        this property cannot be changed after construction
+    */
+    @property ubyte statusByte() const @nogc nothrow pure @safe {
+        return _statusByte;
+    }
+
+    /**
+    Returns:
+        a pointer to the underlying `MIDIEvent`, `SysExEvent` or `MetaEvent`
+        if the object is of that variety; `null` otherwise
+
+    Note:
+        do not reassign the `TrackEvent` object while still having a reference
+        to the object in the union.
+    */
+    inout(MIDIEvent)* asMIDIEvent() inout return @nogc nothrow pure @safe {
+        if (isMIDIEvent(_statusByte)) {
+            return (() inout @trusted => &_midiEvent)();
+        }
+        return null;
+    }
+    /// ditto
+    inout(SysExEvent)* asSysExEvent() inout return @nogc nothrow pure @safe {
+        if (isSysExEvent(_statusByte)) {
+            return (() inout @trusted => &_sysExEvent)();
+        }
+        return null;
+    }
+    /// ditto
+    inout(MetaEvent)* asMetaEvent() inout return @nogc nothrow pure @safe {
+        if (isMetaEvent(_statusByte)) {
+            return (() inout @trusted => &_metaEvent)();
+        }
+        return null;
+    }
+
+private:
+    ubyte _statusByte;
+    union {
+        MIDIEvent _midiEvent;
+        SysExEvent _sysExEvent;
+        MetaEvent _metaEvent;
+    }
+}
+
+/// Here is an example of how to use `TrackEvent`.
+@nogc nothrow pure @safe unittest {
+    auto statusByte = cast(ubyte) ((ChannelMessageType.noteOn << 4) | 0x7);
+    auto event = TrackEvent(
+        200,
+        statusByte,
+        MIDIEvent(statusByte, [0x0F, 0x00]),
+    );
+
+    // get underlying value
+    assert(event.asSysExEvent() is null);
+    assert(event.asMetaEvent() is null);
+    const message = event.asMIDIEvent();
+    assert(message !is null);
+
+    // MIDI event = either a channel message or a system message
+    assert(message.isChannelMessage());
+    assert(!message.isSystemMessage());
+
+    // since it's a channel message, we can use these properties of the
+    // `MIDIEvent` struct:
+    assert(message.getChannelMessageType() == ChannelMessageType.noteOn);
+    assert(message.getChannelNumber() == 0x7);
+    assert(message.data == [0x0F, 0x00]);
+}
+
+/**
+A MIDI event is any normal channel message or system message (except for system
+exclusive events).
+
+See also:
+    `mididi.def.isMIDIEvent`
+*/
+struct MIDIEvent {
+    ///
+    ubyte statusByte;
+
+    /**
+    The data bytes in this message.
+
+    The first bit of both bytes must be 0 and if the message type only needs
+    one byte, the second byte should be `0x00`.
+    */
+    ubyte[2] data;
+
+    /**
+    */
+    bool isChannelMessage() const @nogc nothrow pure @safe {
+        return .isChannelMessage(statusByte);
+    }
+    /// ditto
+    bool isSystemMessage() const @nogc nothrow pure @safe {
+        return .isSystemMessage(statusByte);
+    }
+
+    /**
+    These methods give the channel message type and the channel number (upper
+    and lower four bits, respectively).
+
+    Preconditions:
+        this message must be a channel message, so `this.isChannelMessage()`
+        must be true.
+    See also:
+        `mididi.def.ChannelMessageType`
+    */
+    ChannelMessageType getChannelMessageType() const @nogc nothrow pure @safe
+    in (this.isChannelMessage()) {
+        immutable t = cast(ubyte) (statusByte >> 4);
+        return cast(ChannelMessageType) t;
+    }
+    /// ditto
+    ubyte getChannelNumber() const @nogc nothrow pure @safe
+    in (this.isChannelMessage()) {
+        return statusByte & 0xF;
+    }
+
+    /**
+    Preconditions:
+        this message must be a system message, so `this.isSystemMessage()` must
+        be true.
+    See also:
+        `mididi.def.SystemMessageType`
+    */
+    SystemMessageType getSystemMessageType() const @nogc nothrow pure @safe
+    in (this.isSystemMessage()) {
+        return cast(SystemMessageType) statusByte;
+    }
+}
+
+/**
+A system exclusive event is one unit ("packet") of a system exclusive message,
+which can be used as an escape to emit any arbitrary bytes.
+
+See also:
+    `mididi.def.isSysExEvent`
+*/
+struct SysExEvent {
+    /**
+    The kind of system exclusive event. This can be either `systemExclusive`
+    (`0xF0`) or `endOfExclusive` (`0xF7`).
+    */
+    SystemMessageType type;
+
+    ///
+    int length;
+
+    ///
+    ubyte[] data;
+}
+
+/**
+A meta event is an event that is not regular MIDI message, used to send meta
+information. Not every meta event has to be supported or acted upon.
+
+See Also:
+    `mididi.def.isMetaEvent`; `mididi.def.MetaEventType`
+*/
+struct MetaEvent {
+    ///
+    MetaEventType type;
+
+    ///
+    int length;
+
+    // is usually 1-6 bytes, could also be more data depending if it's a
+    // certain kind of meta event
+    ///
+    ubyte[] data;
+}
